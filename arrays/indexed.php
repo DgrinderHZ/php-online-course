a9f98aa437db8a61eca9564fe07a2632c83945b3
DgrinderHZ <hassanzekkouri@gmail.com>
2023-11-13 14:24:58 +0100

    start arrays

---
+++ arrays/indexed.php
@@ -0,0 +1,10 @@
+<?php
+$names = array("Hassan", "Ali", "Fatima");
+echo "Tableau = ";
+foreach ($names as $value) {
+    echo "$value | ";
+}
+
+// echo "I like " . $names[0] . ", " . $names[1] . " and " . $names[2] . ".";
+
+?>
