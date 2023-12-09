d85cf66d71041f34574feaa6d46ded128064f4ec
DgrinderHZ <hassanzekkouri@gmail.com>
2023-12-09 18:47:04 +0100

    modify test

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+    // Bloc 1
+    echo "Hello World!";
+?>
+<br>
+<?php
+    // Bloc 2
+    echo "PHP: Hypertext Preprocessor!";
+?>
+<p>PHP est gratuit à télécharger et à utiliser</p>
+
+<?php
+    echo("Hello World!");
+?>
